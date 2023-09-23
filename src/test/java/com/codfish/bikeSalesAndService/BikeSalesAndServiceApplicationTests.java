1600442ff6c65206e7e723312ae7310341b2d9b7
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2023-09-23 15:34:01 +0200

    https://start.spring.io/

---
+++ src/test/java/com/codfish/bikeSalesAndService/BikeSalesAndServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codfish.bikeSalesAndService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeSalesAndServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
